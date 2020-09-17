9b4d7e77869668840cc84973a9da814615ec07fc
GitScrubXYZ <41343290+GitScrubXYZ@users.noreply.github.com>
2020-09-17 10:57:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ bot.on('ready', () =>{
 bot.on('message', message => {
     if (message.content === 'ping') {
         message.channel.send('pong');
+        message.channel.send(new Date().getTime() - msg.createdTimestamp + " ms")
     }
 })
 
