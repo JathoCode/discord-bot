a046a8ad7ca1fc4f096c68599a2acba2d6443694
GitScrubXYZ <41343290+GitScrubXYZ@users.noreply.github.com>
2020-09-22 15:39:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PREFIX = ';';
 bot.on('ready', () =>{
   console.log('This bot is online');
   bot.user.setStatus('available')
-  bot.user.setActivity(PREFIX)
+  bot.user.setActivity('Prefix ' + PREFIX)
 })
 
 // T is nu al een zooitje, maar ik maak hier n tijdelijke verzamelplek voor de embeds :)
